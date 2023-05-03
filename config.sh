03dfcedfefd58993690742fc16c7837eddddb8b2
Natalie Forrest <108416595+eenlef@users.noreply.github.com>
2023-05-03 11:49:33 +0100

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,20 @@
+## This will be a script where all the variables can be defined
+
+### Define directories
+# Current working directory
+cwd=
+
+# Nearby directories
+output_rslc=
+output_ifg=
+SLC_orbit=
+output_both=
+
+## Define variables
+track=
+frame=
+superprimary_date=
+both=
+
+## Modules to load
+module load gamma/v?? #edit to version type required
